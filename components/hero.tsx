1af90d178a34ee0fab5860a9f13040246874a4d8
Mohammed Al-Asadi <mohammeasadi@gmail.com>
2026-06-30 20:43:17 -0400

    hero position changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,8 +10,8 @@ export function Hero() {
         <div className="absolute -right-24 top-10 size-[28rem] rounded-full bg-accent/20 blur-3xl" />
         <div className="absolute -bottom-32 left-1/3 size-[26rem] rounded-full bg-primary/10 blur-3xl" />
       </div>
-      <div className="relative mx-auto max-w-6xl px-5 pb-10 pt-16 md:pb-16 md:pt-24">
-        <div className="grid items-center gap-2 lg:grid-cols-[0.5fr_1.5fr]">
+      <div className="relative mx-auto max-w-6xl px-5 pb-16 pt-16 md:pb-20 md:pt-24">
+        <div className="grid items-start gap-15 lg:grid-cols-[0.5fr_1.5fr]">
 
           {/* LEFT SIDE */}
           <div>
@@ -52,7 +52,7 @@ export function Hero() {
           </div>
 
           {/* RIGHT SIDE */}
-          <div className="relative mt-15">
+          <div className="relative mt-10">
             <img
               src="/images/Mask group2.svg"
               alt="Hero illustration"
